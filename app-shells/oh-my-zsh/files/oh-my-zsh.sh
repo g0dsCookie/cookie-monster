8ec8e590e6ba4082f0a7b62a8d11aa6f2a78b673
g0dsCookie <g0dscookie@cookieprojects.de>
2020-07-08 10:00:23 +0200

    Removed unused packages

--- app-shells/oh-my-zsh/files/oh-my-zsh.sh
+++
@@ -1,9 +0,0 @@
-export ZSH=/usr/share/zsh/oh-my-zsh
-export ZSH_CACHE_DIR=$HOME/.cache/zsh/oh-my-zsh
-export ZSH_CUSTOM=$HOME/.local/share/zsh/oh-my-zsh
-
-# create missing directories
-[[ ! -e "${ZSH_CACHE_DIR}" ]] && mkdir -p "${ZSH_CACHE_DIR}"
-[[ ! -e "${ZSH_CUSTOM}" ]] && mkdir -p "${ZSH_CUSTOM}"
-
-source $ZSH/oh-my-zsh.sh
